2f8d5a3444260a96d72818fa67c0e0e1fe709c2c
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-04 14:25:10 -0600

    insert new products into the products table

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -12,3 +12,33 @@ CREATE TABLE products (
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Iphone X", "Electronics", 925.50, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Samsung Galaxy 10", "Electronics", 720.00, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("MacBook Pro", "Electronics", 1825.50, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dinner Table", "Home", 250.00, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Microwave", "Home", 125.00, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Notebook", "Office", 4.75, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Desk Chair", "Office", 44.99, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Stroller", "Baby", 144.75, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Car Seat", "Baby", 64.98, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pajama", "Baby", 2.75, 30);

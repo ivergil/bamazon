cc9e4d249c7e8f1ca6f8dba0244fd820bf85f91e
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-04 14:55:41 -0600

    Creating the Customer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,34 +3,39 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "bamazon_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     initBamazon();
-  });
+});
 
-function initBamazon(){
+function initBamazon() {
     console.log("connected as id " + connection.threadId + "\n");
-    connection.query("SELECT * FROM bamazon_db", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-  
+
         for (var i = 0; i < res.length; i++) {
-          console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price );
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
         }
-        // console.log("-----------------------------------");
-      });
+        console.log("-----------------------------------");
+        
+    });
+    buyProducts();
+};
+
+function buyProducts(){
+    inquirer
+    .prompt({
+      name: "buy",
+      type: "input",
+      message: "What is the item you would like to Buy?:  ",
+    })
 }

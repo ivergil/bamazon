b7ef1cd5548fa6412abc1f8cb8cd01d5488e56af
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-04 14:01:37 -0600

    Create init function for bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,3 +15,14 @@ var connection = mysql.createConnection({
   password: "password",
   database: "bamazon_db"
 });
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    initBamazon();
+  });
+
+function initBamazon(){
+    
+}

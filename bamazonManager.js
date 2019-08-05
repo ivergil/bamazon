2d4432f0c6d84d93e58204777bee9d9e7bb986eb
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-04 19:38:48 -0600

    Create new fucntion addInventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -30,22 +30,18 @@ function initManager() {
             switch (choosen.managerTask) {
                 case "View Products for Sale":
                     viewProducts();
-                    connection.end();
                     break;
 
                 case "View Low Inventory":
                     viewLowInventory();
-                    connection.end();
                     break;
 
                 case "Add to Inventory":
                     addInventory();
-                    connection.end();
                     break;
 
                 case "Add New Product":
                     addNewProduct();
-                    connection.end();
                     break;
                 default: connection.end();
             }
@@ -64,13 +60,10 @@ function viewProducts() {
             table.push(
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].stock_quantity, res[i].price]
             )
-            // console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].stock_quantity + " | " + res[i].price);
         }
-        // console.log("-----------------------------------");
         console.log(table.toString());
+        initManager();
     });
-
-    console.log("entro por ver productos")
 
 }
 
@@ -90,13 +83,64 @@ function viewLowInventory() {
         }
         // console.log("-----------------------------------");
         console.log(table.toString());
+        initManager();
     });
 }
 
 function addInventory() {
-    console.log("Entro por Agregar Inventario")
-}
+    var table = new Table({
+        head: ["ID", "Product Name", "Departament", "Stock", "Price $",]
+        , colWidths: [5, 20, 20, 10, 10]
+    });
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].stock_quantity, res[i].price]
+            )
+        }
+        console.log(table.toString());
+
+        inquirer
+            .prompt([
+                {
+                    name: "item",
+                    type: "input",
+                    message: "What item would you like to add stock (ID)?: "
+                },
+                {
+                    name: "qty",
+                    type: "input",
+                    message: "Please insert the stock to add: "
+                },
+            ])
+            .then(function (ans) {
+                connection.query("SELECT * FROM products", function (err, resDB) {
+                    var i = ans.item - 1;
+                    var newQ = parseInt(ans.qty) + parseInt(resDB[i].stock_quantity);
+                    console.log(newQ);
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newQ
+                            },
+                            {
+                                item_id: ans.item
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Add new Stock successfully!");
+                            console.log("The new Inventory for " + resDB[i].product_name + " has been updated from " + resDB[i].stock_quantity + " to " + newQ );
+                        }
+                    );
+                })
+            })        
+    });
+    
+};
 
 function addNewProduct() {
-    console.log("Entro por Agregar nuevo PRODUCTO")
-}
+    console.log("Entro por Agregar nuevo PRODUCTO");
+};
